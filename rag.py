0f8d0346978d56ac09cfea1d9b6e2c68d0cad1f7
anushaphougat <anushaphougat@gmail.com>
2026-08-06 02:37:01 +0530

    Update rag.py

--- rag.py
+++ rag.py
@@ -1,5 +1,5 @@
 from langchain_community.vectorstores import FAISS
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEndpointEmbeddings
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 from utils import format_llm_response
@@ -11,7 +11,10 @@ llm = ChatGroq(
     model="llama-3.1-8b-instant",
     api_key=os.getenv("GROQ_API_KEY")
 )
-embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+embedding_model = HuggingFaceEndpointEmbeddings(
+    model="sentence-transformers/all-MiniLM-L6-v2",
+    huggingfacehub_api_token=os.getenv("HF_API_TOKEN")
+)
 
 retriever = None
 
